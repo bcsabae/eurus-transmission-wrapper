4e0d70ecc4be73b373abd0dc3e8a2d3f73a235be
bcsabae <berky.csaba@outlook.com>
2022-09-11 15:16:58 +0200

    Implement API endpoints

--- WrappedClient.py
+++ WrappedClient.py
@@ -1,18 +1,115 @@
+from typing import Any
 from transmission_rpc import Client
 from transmission_rpc import Torrent
 from transmission_rpc.torrent import Status
+import transmission_rpc.error as error
 from urllib.parse import urlparse
+import json
 
 class WrappedClient():
     _client = None
+    _config = None
+    _config_file = None
+    _connected = None
+    _authenticated = None
 
-    def __init__(self, url, username=None, password=None) -> None:
-        self._url = url
+    def __init__(self, username=None, password=None, config_file='config.json') -> None:
+        self._config_file = config_file
         self._username = username
         self._password = password
-        parsed_url = urlparse(url)
-        self._client = Client(host=parsed_url.hostname, port=parsed_url.port, path=parsed_url.path, username=username, password=password)
+
+        # TODO: check if config file is writeable
+        try:
+            with open(self._config_file) as cf:
+                self._config = json.load(cf)
+        except FileNotFoundError:
+            # LOG: config file not found
+            print("file not found")
+            return
+
+        self.connect()
+        
         return
+
+    def connect(self) -> bool:
+        self._url = self.get_config('server_address')
+        if self._url == None:
+            # LOG: not a valid URL
+            print("not a valid URL")
+            self._connected = False
+            return False
+
+        parsed_url = urlparse(self._url)
+        if parsed_url.scheme != 'http' or not parsed_url.netloc:
+            # LOG
+            print("not a valid url")
+            self._connected = False
+            return False
+
+        try:
+            self._client = Client(host=parsed_url.hostname, port=parsed_url.port, path=parsed_url.path, username=self._username, password=self._password)
+            self._connected = True
+            self._authenticated = True
+            return True
+        except error.TransmissionConnectError:
+            # LOG: cannot connect to server
+            print("cannot connect to server")
+            self._connected = False
+            return False
+        except error.TransmissionAuthError:
+            # LOG: authentication failure
+            print("authentication failure")
+            self._connected = True
+            self._authenticated = False
+            return False
+        return False
+
+    def connection_needed(func) -> Any:
+        def wrapper(*args, **kwargs) -> Any:
+            # check connection
+            if args[0].is_connected():
+                return func(*args, **kwargs)
+            else:
+                print("not connected")
+                return None
+        return wrapper
+
+    def _write_config(self) -> bool:
+        try:
+            with open(self._config_file, 'w') as cf:
+                json.dump(self._config, cf)
+        except FileNotFoundError:
+            return False
+        except PermissionError:
+            return False
+        return True
+
+    def get_config(self, key: str = None) -> Any:
+        if key == None:
+            print(self._config)
+            return self._config
+        try:
+            value = self._config[key]
+        except KeyError:
+            value = None
+        return value
+
+    def set_config(self, key: str, value: str) -> Any:
+        if key not in self._config:
+            return None
+        self._config[key] = value
+        could_write = self._write_config()
+        if key == "server_address":
+            self.connect()
+        
+        return value if could_write else False
+
+    def is_connected(self) -> bool:
+        return self._connected if self._connected != None else False
+    
+    def is_authenticated(self) -> bool:
+        return self._authenticated if self._authenticated != None else False
+
 
     @staticmethod
     def status_map(status: Status) -> int:
@@ -43,6 +140,7 @@ class WrappedClient():
         torrent_dict['downloadDir'] = torrent.download_dir
         return torrent_dict
 
+    @connection_needed
     def get_torrents(self) -> list:
         torrents = self._client.get_torrents()
         torrents_mapped = []
@@ -50,6 +148,30 @@ class WrappedClient():
             torrents_mapped.append(self.torrent_map(torrent))
         return torrents_mapped
 
+    @connection_needed
     def get_torrent(self, id: int) -> dict:
+        try:
+            torrent = self._client.get_torrent(id)
+        except KeyError:
+            return None
+        return self.torrent_map(torrent)
+
+    @connection_needed
+    def stop_torrent(self, id: int) -> dict:
+        try:
+            torrent = self._client.get_torrent(id)
+        except KeyError:
+            return None
+        torrent.stop()
+        torrent = self._client.get_torrent(id)
+        return self.torrent_map(torrent)
+
+    @connection_needed
+    def start_torrent(self, id: int) -> dict:
+        try:
+            torrent = self._client.get_torrent(id)
+        except KeyError:
+            return None
+        torrent.start()
         torrent = self._client.get_torrent(id)
         return self.torrent_map(torrent)
--- main.py
+++ main.py
@@ -1,7 +1,108 @@
+import json
+from textwrap import wrap
+from typing import Any
 from flask import Flask
+from flask import request
+from WrappedClient import WrappedClient
+import json
 
 app = Flask(__name__)
+client = WrappedClient(username="transmission", password="transmission")
 
-@app.route('/')
-def test():
-    return "Hello world!"
+def response(status: str, data: Any=None) -> str:
+    response_json = {
+        'status': status,
+        'data': data if data != None else {}
+    }
+    return json.dumps(response_json)
+
+def online(func) -> Any:
+    def wrapper(*args, **kwargs) -> Any:
+        if not client.is_connected():
+            return response('not connected')
+        if not client.is_authenticated():
+            return response("not authenticated")
+        return func(*args, **kwargs)
+    return wrapper
+
+def connected(func) -> Any:
+    def wrapper(*args, **kwargs) -> Any:
+        if not client.is_connected():
+            return response('not connected')
+        return func(*args, **kwargs)
+    return wrapper
+
+def validate_id(func) -> Any:
+    def wrapper(*args, **kwargs) -> Any:
+        if 'id' in kwargs:
+            id = kwargs['id']
+        else:
+            id = args[0]
+        try:
+            id_int = int(id)
+            if id_int < 0:
+                raise ValueError
+        except ValueError:
+            return response('invalid id format')
+        return func(*args, **kwargs)
+    return wrapper
+
+@app.get('/status', endpoint='status')
+@online
+def status():    
+    return response("success")
+
+@app.get('/torrents', endpoint='get_torrents')
+@online
+def get_torrents():
+    torrents = client.get_torrents()
+    if torrents != None:
+        return response('success', torrents)
+    else:
+        return response('request error')
+
+@app.get('/torrents/<id>', endpoint='get_torrent')
+@online
+@validate_id
+def get_torrent(id=id):  
+    torrent = client.get_torrent(int(id))
+    if torrent == None:
+        return response('not found')
+    return response('success', torrent)
+
+@app.get('/torrents/<id>/stop', endpoint='stop_torrent')
+@online
+@validate_id
+def stop_torrent(id=id):
+    torrent = client.stop_torrent(int(id))
+    if torrent == None:
+        return response('not found')
+    return response('success', torrent)
+
+@app.get('/torrents/<id>/start', endpoint='start_torrent')
+@online
+@validate_id
+def stop_torrent(id=id):
+    torrent = client.start_torrent(int(id))
+    if torrent == None:
+        return response('not found')
+    return response('success', torrent)
+
+@app.get('/config', endpoint='get_config')
+def get_config():
+    config = client.get_config()
+    return response('success', config)
+
+@app.post('/config', endpoint='set_config')
+def set_config():
+    missing_key = False
+    requested_config = request.get_json(force=True)
+    for key in requested_config:
+        print(f'reuqesting to change {key}')
+        if client.set_config(key, requested_config[key]) == None:
+            missing_key = True
+    
+    if missing_key:
+        return response('missing key', client.get_config())
+    else:
+        return response('success', client.get_config())
