a6efd9b8f005f228eb14ebe040a20d96ab1df593
bcsabae <berky.csaba@outlook.com>
2022-09-11 23:30:50 +0200

    update test script

--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 from WrappedClient import WrappedClient
 
-client = WrappedClient("http://192.168.2.100:9091/transmission/rpc", username="transmission", password="transmission")
+client = WrappedClient(username="transmission", password="transmission")
 print(client.get_torrents())
+client.delete_torrent(2)
